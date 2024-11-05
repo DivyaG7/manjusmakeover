de5f8e3591ec9f2f292db46482ef17e5d6d46664
unknown <divyaneela75@gmail.com>
2024-11-05 15:45:33 +0530

    first commit

--- src/components/Hcontact.jsx
+++ src/components/Hcontact.jsx
@@ -13,7 +13,7 @@ export const Hcontact = () => {
         const formData = { name, phone, service, email };
 
         try {
-            const response = await fetch('http://localhost:5000/api/send_email', {
+            const response = await fetch('https://manjusmakeover.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
